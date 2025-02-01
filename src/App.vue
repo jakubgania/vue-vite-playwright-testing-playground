cb26d6797f13ba7357ab74bb7f5c8191822e532d
Jakub Gania <contact@jakubgania.io>
2025-02-01 23:45:06 +0100

    development

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,20 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <!-- sidebar -->
+  <div class="sidebar">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/next">Next</RouterLink>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 
-  <RouterLink to="/">Home</RouterLink>
-  <RouterLink to="/next">Next</RouterLink>
-
-  <RouterView />
+  <!-- content -->
+  <div>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.sidebar {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
 }
 </style>
